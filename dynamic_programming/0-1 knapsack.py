13078974512384ce7fe1c81dff46403e9c88f357
iamsankalp <mcs2019009@iiitl.ac.in>
2021-05-07 10:52:30 +0530

    memoized solution also done

--- dynamic_programming/0-1 knapsack.py
+++ dynamic_programming/0-1 knapsack.py
@@ -14,6 +14,28 @@ value = [10, 30, 40, 100, 26, 15]
 n = len(weight)
 
 ans = knapsack(capacity, weight, value, n)
-print(ans)
+print("Recursive solution: ", ans)
 
-    
+
+#memoized solution
+
+def memoized_knapsack(capacity, weight, value, n):
+    if n == 0 or capacity == 0: #base condition
+        dp[n][capacity] = 0
+        return 0
+    if dp[n][capacity] != -1:
+        return dp[n][capacity]
+    if weight[n-1] <= capacity:
+        dp[n][capacity]= max((value[n-1] + memoized_knapsack(capacity-weight[n-1],weight,value,n-1)),memoized_knapsack(capacity,weight,value,n-1))
+        return dp[n][capacity]
+    else:
+        dp[n][capacity]= memoized_knapsack(capacity,weight,value,n-1)
+        return dp[n][capacity]
+
+capacity = 100
+weight = [10, 20, 30, 10, 20 , 25]
+value = [10, 30, 40, 100, 26, 15]
+n = len(weight)
+dp = [[-1]*(capacity+1)]*(n+1)
+ans = memoized_knapsack(capacity, weight, value, n)
+print("Memoized solution: ", ans)
