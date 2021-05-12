8f4837be77bd352d60deeb687bbfbc48d9ddc75e
iamsankalp <mcs2019009@iiitl.ac.in>
2021-05-12 11:47:18 +0530

    rod_cutting

--- dynamic_programming/rod_cutting(unbounded_knapsack).py
+++ dynamic_programming/rod_cutting(unbounded_knapsack).py
@@ -19,6 +19,34 @@ print(ans)
 #tabulation
 
 """
-
+Only two attributes that varies are the length(length) and length of array(n)
+Output is the max profit which will be the value stored in the table
 """
+
+# dp = [[-1]*(length+1)]*(n+1)
+rows, cols = (5, 5)
+dp = [[-1 for i in range(length+1)] for j in range(n+1)]    #
+# gives us a table with row as length and columns as different length of array
+
+
+#initialise
+
+for i in range(length+1):
+    dp[0][i] = 0    #n=0 all rod length should give 0
+for j in range(n+1):
+    dp[j][0] = 0    #if length of rod is 0, profit will always be 0
+
+for n in range(1,n+1):
+    for length in range(1,length+1):
+        if pieces[n-1] > length:
+            dp[n][length] = dp[n-1][length]
+        else:
+            dp[n][length] = max(value[n-1]+dp[n][length-pieces[n-1]], dp[n-1][length])
+
+ans = dp[n][length]
+print (ans)
+print("table of dp")
+for i in dp:
+    print(i)
+
 
