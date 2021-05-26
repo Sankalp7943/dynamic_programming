f5e47145aa813c9658d989e4cc75be22885fcade
iamsankalp <mcs2019009@iiitl.ac.in>
2021-05-26 10:50:03 +0530

    more optimised memoized solution

--- dynamic_programming/minimumPartitionPalindrome.py
+++ dynamic_programming/minimumPartitionPalindrome.py
@@ -39,3 +39,36 @@ def solve_dp(string, i, j):
 print(solve_dp(string, 0, len(string)-1))
 for i in dp:
     print(i)
+
+# further optimised
+
+string = "nitik"
+dp = [[-1 for i in range(len(string)+1)] for j in range(len(string)+1)]
+
+def solve_dp2(string, i, j):
+    if dp[i][j] != -1:
+        return dp[i][j]
+    if i >= j:
+        dp[i][j] = 0
+        return dp[i][j]
+    if string[i:j]==string[j:i:-1]:
+        dp[i][j] = 0
+        return dp[i][j]
+    ans = float('inf')
+    for k in range(i,j,1):  #i to j-1
+        if dp[i][k]!= -1:
+            left = dp[i][k]
+        else:
+            left = solve_dp2(string, i, k)
+        if dp[k+1][j]!= -1:
+            right = dp[k+1][j]
+        else:
+            right = solve_dp2(string, k+1, j)
+        tempans = left + right + 1
+        ans = min(ans, tempans)
+        dp[i][j] = ans
+    return dp[i][j]
+
+print(solve_dp2(string, 0, len(string)-1))
+for i in dp:
+    print(i)
