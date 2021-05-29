4df0dafef146bc62ff66df37390adf20fcbc3968
Sankalp Sharma <sharmasankalp@outlook.com>
2021-05-29 10:28:49 +0530

    memoized solution

--- dynamic_programming/scrambledString.py
+++ dynamic_programming/scrambledString.py
@@ -24,3 +24,26 @@ def isScramble(s1: str, s2: str) -> bool:
     return False
 
 print(isScramble(a,b))
+
+#memoize
+dic={}
+a = "great"
+b = "ategr"
+def isScrambleDP(s1: str, s2: str) -> bool:
+    if (s1, s2) in dic :
+        return dic[(s1, s2)]
+    if len(s1) != len(s2) or sorted(s1) != sorted(s2):
+        dic[(s1, s2)] = False     
+        return False
+    n = len(s1)
+    if s1 == s2:
+        dic[(s1, s2)] = True
+        return True
+    for i in range(1, n):
+        if (isScrambleDP(s1[i:], s2[i:]) and isScrambleDP(s1[:i], s2[:i])):
+            return True
+        if (isScrambleDP(s1[:-i], s2[i:]) and isScrambleDP(s1[-i:], s2[:i])):
+            return True
+    dic[(s1, s2)] = False
+    return False
+print(isScrambleDP(a,b))
